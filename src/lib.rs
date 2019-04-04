66e08d9202ecd380e8cba242a42a72a8c43cd515
Maciej Hirsz <maciej.hirsz@gmail.com>
2019-04-04 11:25:42 +0200

    chore: doc comment with example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! # blockies.rs
+//!
+//! library that generates blocky identicons
+//!
+//! Rust implementation of javascript [blockies](https://github.com/download13/blockies) library. Supports also [ethereum blockies](https://github.com/alexvandesande/blockies)
+//!
+//! ### Library usage
+//!
+//! ```rust
+//! use blockies::Ethereum;
+//!
+//! let blockies = Ethereum::default();
+//! let mut png = Vec::new();
+//!
+//! blockies.create_icon(&mut png, b"0x01122df2b7d1c0a6ad94589da045af3885bedbbc");
+//!
+//! // `png` now contains a rendered image of the blockies for that address
+//! assert_eq!(png.len(), 179);
+//! ```
+
 mod classic;
 mod ethereum;
 pub(crate) mod util;
