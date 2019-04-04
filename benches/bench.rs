ccaddbde2004b080353dadab63f425234aa141b1
Maciej Hirsz <maciej.hirsz@gmail.com>
2019-04-04 11:23:40 +0200

    fix: CLI after all the changes

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,25 +2,18 @@
 extern crate test;
 
 use test::{Bencher, black_box};
+use blockies::Ethereum;
 
 #[bench]
 fn generate(b: &mut Bencher) {
-    use blockies::{create_icon, ethereum};
-
     let seed = b"\x01\x12\x2d\xf2\xb7\xd1\xc0\xa6\xad\x94\x58\x9d\xa0\x45\xaf\x38\x85\xbe\xdb\xbc";
+    let blockies = Ethereum::default();
 
     b.bytes = 745;
 
     b.iter(|| {
         let mut memory = Vec::with_capacity(1024);
-        let options = ethereum::Options {
-            size: 8,
-            scale: 16,
-            color: None,
-            background_color: None,
-            spot_color: None,
-        };
 
-        black_box(create_icon(&mut memory, seed, options).unwrap());
+        black_box(blockies.create_icon(&mut memory, seed).unwrap());
     });
 }
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,36 +1,30 @@
 #[macro_use]
 extern crate clap;
-extern crate blockies;
 
 use std::fs::File;
-use blockies::{Blockies, ethereum, classic, create_icon};
-
-struct Config {
-	blockies: Blockies,
-	file: File,
-}
+use blockies::{Ethereum, Classic};
 
 fn main() {
 	match run() {
 		Ok(_) => (),
 		Err(err) => {
-			println!("{}", err);
+			eprintln!("{}", err);
 		}
 	}
 }
 
-fn get_config() -> Result<Config, String> {
+fn run() -> Result<(), String> {
 	let yaml = load_yaml!("cli.yml");
 	let matches = clap::App::from_yaml(yaml).get_matches();
 
-	let size: u32 = match matches.value_of("size") {
+	let size: usize = match matches.value_of("size") {
 		Some(size) => size.parse().map_err(|_| "Invalid --size")?,
-		None => 10,
+		None => 8,
 	};
 
-	let scale: u32 = match matches.value_of("scale") {
+	let scale: usize = match matches.value_of("scale") {
 		Some(scale) => scale.parse().map_err(|_| "Invalid --scale")?,
-		None => 5,
+		None => 16,
 	};
 
 	let seed: Vec<u8> = match matches.value_of("seed") {
@@ -38,43 +32,35 @@ fn get_config() -> Result<Config, String> {
 		None => { unreachable!() }
 	};
 
-	let blockies = match matches.value_of("mode") {
-		Some("ethereum") => Blockies::Ethereum(ethereum::Options {
-			size: size,
-			scale: scale,
-			seed: seed,
-			color: None,
-			background_color: None,
-			spot_color: None,
-		}),
-		Some("classic") | None => Blockies::Classic(classic::Options {
-			size: size,
-			scale: scale,
-			seed: seed,
-			color: None,
-			background_color: None,
-		}),
-		_ => {
-			return Err("Invalid --mode".to_owned());
-		},
-	};
-
 	let output_filename = match matches.value_of("output") {
 		Some(filename) => filename,
 		None => "blockies.png",
 	};
 
-	let file = File::create(output_filename).map_err(|_| format!("Could not create file {}", output_filename))?;
 
-	let config = Config {
-		blockies: blockies,
-		file: file,
-	};
-	Ok(config)
-}
+	match matches.value_of("mode") {
+		Some("ethereum") => {
+			let mut blockies = Ethereum::default();
 
-fn run() -> Result<(), String>  {
-	let mut config = get_config()?;
-	create_icon(&mut config.file, config.blockies).map_err(|_| "Unexpected error")?;
-	Ok(())
+			blockies.size = size;
+			blockies.scale = scale;
+
+			let file = File::create(output_filename).map_err(|_| format!("Could not create file {}", output_filename))?;
+
+			blockies.create_icon(file, &seed).map_err(|err| format!("Could not render: {:?}", err))
+		},
+		Some("classic") | None => {
+			let mut blockies = Classic::default();
+
+			blockies.size = size;
+			blockies.scale = scale;
+
+			let file = File::create(output_filename).map_err(|_| format!("Could not create file {}", output_filename))?;
+
+			blockies.create_icon(file, &seed).map_err(|err| format!("Could not render: {:?}", err))
+		},
+		_ => {
+			Err("Invalid --mode".to_owned())
+		},
+	}
 }
